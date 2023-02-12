0c1d7f35bc6b8a2c467d647c390607d49d906f43
OrestShylan <orestshylan1997@gmail.com>
2023-02-12 18:55:11 +0200

    add render

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import './css/styles.css';
+import fetchCounry from './js/fetchCountry';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import renderCountries from './js/renderCountries';
+
+const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+input.addEventListener('input', debounce(searchCountries, DEBOUNCE_DELAY));
+
+function searchCountries(event) {
+  let countryName = event.target.value.trim();
+  if (countryName) {
+    fetchCounry(countryName)
+      .then(dataCountries => {
+        renderCountries(dataCountries);
+      })
+      .catch(error => {
+        Notify.failure('Oops, there is no country with that name');
+      });
+  } else {
+    countryInfo.innerHTML = '';
+    countryList.innerHTML = '';
+  }
+}
---
+++ src/js/renderCountries.js
@@ -0,0 +1,38 @@
+import { Notify } from 'notiflix';
+export default function renderCountries(dataCountries) {
+  const countryList = document.querySelector('.country-list');
+  const countryInfo = document.querySelector('.country-info');
+  let quantityCountries = dataCountries.length;
+  let countryItems = [];
+  let currentCountry = dataCountries[0];
+
+  if (quantityCountries > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+  } else if (quantityCountries >= 2 && quantityCountries <= 10) {
+    dataCountries.map(country => {
+      countryItems.push(`
+        <li class="wrap-flag_item">
+          <img src='${country.flags.svg}' alt='' width=40 class="flags"/>
+          <span>${country.name.official}</span>
+        </li>`);
+    });
+    countryInfo.innerHTML = '';
+    countryList.innerHTML = countryItems;
+  } else {
+    countryInfo.innerHTML = `
+          <div class="wrap_flag">
+            <img src='${
+              currentCountry.flags.svg
+            }' alt='' class="flags" width=40 />
+          <span class="capital">${currentCountry.name.official}</span>
+        </div>
+            <p>Capital: ${currentCountry.capital}</p>
+            <p>Population: ${currentCountry.population}</p>
+            <p>Languages: ${Object.values(currentCountry.languages).join(
+              ', '
+            )}</p>
+        
+    `;
+    countryList.innerHTML = '';
+  }
+}
